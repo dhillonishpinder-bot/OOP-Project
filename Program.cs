228f1328400bf6c0c0f98ea25fd240ed829f5ce1
amritkaur0510 <amritkaur.mailbox@gmail.com>
2026-06-12 00:38:37 -0600

    Initial Connect Four project setup
    Created project structure and added classes for Controller, GameBoard, Player, HumanPlayer, and View.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConnectFour
+{
+    public class Controller
+    {
+        private GameBoard board;
+
+        public Controller()
+        {
+            board = new GameBoard();
+        }
+    }
+
+    public class GameBoard
+    {
+        private char[,] board;
+
+        public GameBoard()
+        {
+            board = new char[6, 7];
+        }
+    }
+
+    public abstract class Player
+    {
+        public string Name { get; set; } = "";
+        public char Symbol { get; set; }
+    }
+
+    public class HumanPlayer : Player
+    {
+    }
+
+    public class View
+    {
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Controller game = new Controller();
+
+            Console.WriteLine("Connect Four");
+        }
+    }
+}
