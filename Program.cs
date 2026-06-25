106a74cfab0fd73912d450d19cb73efcf247c459
amritkaur0510 <amritkaur.mailbox@gmail.com>
2026-06-25 17:16:01 -0600

    Completed GameBoard

--- Program.cs
+++ Program.cs
@@ -42,6 +42,25 @@ namespace ConnectFour
             }
         }
     }
+
+        public class GameBoard
+    {
+        private const int Rows = 6;
+        private const int Columns = 7;
+        private char[,] board;
+
+        public GameBoard()
+        {
+            board = new char[Rows, Columns];
+
+            for (int row = 0; row < Rows; row++)
+            {
+                for (int col = 0; col < Columns; col++)
+                {
+                    board[row, col] = '.';
+                }
+            }
+        }
     public class Controller
     {
         private GameBoard board;
@@ -83,43 +102,9 @@ namespace ConnectFour
     }
 }
 
-using System;
 
-namespace ConnectFour
-{
-    // Player Abstract Class
-    public abstract class Player
-    {
-        public string Name { get; private set; }
-        public char Symbol { get; private set; }
-
-        public Player(string name, char symbol)
-        {
-            Name = name;
-            Symbol = symbol;
-        }
-
-        public abstract int GetMove();
-    }
 
 
 
      // GameBoard Class
-    public class GameBoard
-    {
-        private const int Rows = 6;
-        private const int Columns = 7;
-        private char[,] board;
 
-        public GameBoard()
-        {
-            board = new char[Rows, Columns];
-
-            for (int row = 0; row < Rows; row++)
-            {
-                for (int col = 0; col < Columns; col++)
-                {
-                    board[row, col] = '.';
-                }
-            }
-        }
